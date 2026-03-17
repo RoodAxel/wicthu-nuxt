422a0c2b18f5c8ea4f576a511c09bd418c92cb8c
axel <axel.rood@outlook.fr>
2026-03-17 10:56:42 +0100

    enhanced performance

--- server/api/ouvrage-mythe/[id].get.ts
+++ server/api/ouvrage-mythe/[id].get.ts
@@ -9,10 +9,24 @@ export default defineEventHandler(async (event) => {
 
   const ouvrage = await prisma.ouvrage_mythe.findUnique({
     where: { id: Number(id) },
-    include: {
+    select: {
+      id: true,
+      titre: true,
+      auteur: true,
+      langue: true,
+      date: true,
+      description: true,
+      sante_mental: true,
+      gain_mythe_initial: true,
+      gain_mythe_complet: true,
+      mythe_cthulhu: true,
+      semaine: true,
       ouvrage_sort: {
         orderBy: { nom_dans_ouvrage: 'asc' },
-        include: {
+        select: {
+          id: true,
+          nom_dans_ouvrage: true,
+          note: true,
           sort: { select: { id: true, name: true } }
         }
       }
